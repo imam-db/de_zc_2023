d9b73be22b1ac68cdc49ddce8f1f8547254acaa1
Imam <imam0067@gmail.com>
2023-01-26 14:20:55 +0700

    update hw 1 sql; add gitignore

--- week_1/homework.md
+++ week_1/homework.md
@@ -79,9 +79,11 @@ Tip: started and finished on 2019-01-15.
 Remember that `lpep_pickup_datetime` and `lpep_dropoff_datetime` columns are in the format timestamp (date and hour+min+sec) and not in date.
 
 - 20689
-- 20530
+- 20530 **
 - 17630
 - 21090
+
+**Explanation**
 
 ## Question 4. Largest trip for each day
 
@@ -90,8 +92,10 @@ Use the pick up time for your calculations.
 
 - 2019-01-18
 - 2019-01-28
-- 2019-01-15
+- 2019-01-15 **
 - 2019-01-10
+
+**Explanation**
 
 ## Question 5. The number of passengers
 
@@ -99,8 +103,10 @@ In 2019-01-01 how many trips had 2 and 3 passengers?
  
 - 2: 1282 ; 3: 266
 - 2: 1532 ; 3: 126
-- 2: 1282 ; 3: 254
+- 2: 1282 ; 3: 254 **
 - 2: 1282 ; 3: 274
+
+**Explanation**
 
 
 ## Question 6. Largest tip
@@ -113,17 +119,6 @@ Note: it's not a typo, it's `tip` , not `trip`
 - Central Park
 - Jamaica
 - South Ozone Park
-- Long Island City/Queens Plaza
+- Long Island City/Queens Plaza **
 
-
-## Submitting the solutions
-
-* Form for submitting: [form](https://forms.gle/EjphSkR1b3nsdojv7)
-* You can submit your homework multiple times. In this case, only the last submission will be used. 
-
-Deadline: 26 January (Thursday), 22:00 CET
-
-
-## Solution
-
-We will publish the solution here
+**Explanation**
---
+++ week_1/query.sql
@@ -0,0 +1,36 @@
+-- How many taxi trips were totally made on January 15?
+
+SELECT COUNT(*) AS total_trips
+FROM public.tripdata AS td
+WHERE td.lpep_pickup_datetime BETWEEN  '2019-01-15 00:00:00' AND '2019-01-15 23:59:59'
+AND td.lpep_dropoff_datetime BETWEEN  '2019-01-15 00:00:00' AND '2019-01-15 23:59:59';
+
+-- Which was the day with the largest trip distance? Use the pick up time for your calculations.
+
+SELECT td.lpep_pickup_datetime::date AS date_trip,
+	MAX(trip_distance) AS largest_distance
+FROM public.tripdata AS td
+GROUP BY td.lpep_pickup_datetime::date
+ORDER BY largest_distance DESC
+LIMIT 1;
+
+-- In 2019-01-01 how many trips had 2 and 3 passengers?
+
+SELECT COUNT(CASE WHEN td.passenger_count = 2 THEN passenger_count END) AS passenger_2,
+	COUNT(CASE WHEN td.passenger_count = 3 THEN passenger_count END) AS passenger_3
+FROM public.tripdata AS td
+WHERE td.lpep_pickup_datetime::date = '2019-01-01';
+
+-- For the passengers picked up in the Astoria Zone which was the drop off zone that had the largest tip? 
+-- We want the name of the zone, not the id.
+
+SELECT tz_do."Zone" AS dropout_zone,
+	MAX(td.tip_amount) AS max_tip
+FROM public.tripdata AS td
+INNER JOIN public.taxi_zone AS tz_pu
+	ON td."PULocationID" = tz_pu."LocationID"
+INNER JOIN public.taxi_zone AS tz_do
+	ON td."DOLocationID" = tz_do."LocationID"
+WHERE tz_pu."Zone" = 'Astoria'
+GROUP BY tz_do."Zone"
+ORDER BY max_tip DESC;
